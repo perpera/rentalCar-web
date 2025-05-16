4c7ac2f0033a74053b1775f80cd9949b72afba6f
perpera <poalina@icloud.com>
2025-05-17 00:46:16 +0300

    added 404 page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import { lazy} from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const Hometab = lazy(() => import('../pages/Hometab/Hometab'));
+const NotFound = lazy(() => import('../pages/404page/404'));
 
 export default function App() {
     return (
         <Routes>
             <Route index element={<Hometab/>}/>
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
