7b04d0248b71ed59d5cdc1588b1add66ffd9096a
perpera <poalina@icloud.com>
2025-05-16 16:13:46 +0300

    created header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,13 @@
-export default function App() {}
+import { lazy} from 'react';
+/* import { useDispatch, useSelector } from 'react-redux'; */
+import { Route, Routes } from 'react-router-dom';
+
+const Hometab = lazy(() => import('../components/Hometab/Hometab'));
+
+export default function App() {
+    return (
+        <Routes>
+            <Route index element={<Hometab/>}/>
+        </Routes>
+    );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import logo from '../../assets/logo.svg';
+import styles from './Header.module.css';
+
+export default function Header() {
+    return (
+        <>
+        <header className={styles.header}>
+       <NavLink to='/'> <img src={logo} alt="Logo"/></NavLink>
+       <nav className={styles.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.active : styles.link)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) => (isActive ? styles.active : styles.link)}
+        >
+          Catalog
+        </NavLink>
+      </nav>
+        </header>
+        </>
+    );
+}
