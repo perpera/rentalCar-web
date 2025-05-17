ba5a82025f5c8cba8f8685a0c0c94f3bce80ec84
perpera <poalina@icloud.com>
2025-05-18 00:11:56 +0300

    added Loader

---
+++ src/ui/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+export default function Loader() {
+    return (
+        <div class="page">
+            <header class="header">
+		<h1 class="header-title">Loading, please wait</h1>
+	</header>
+    <main class="container">
+		<div class="item">
+			<i class="loader --4"></i>
+		</div>
+	</main>
+</div>
+    );
+}
