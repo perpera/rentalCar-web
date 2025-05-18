9175ba296cba542b104d29b0197e377ca3df380e
perpera <poalina@icloud.com>
2025-05-18 21:01:38 +0300

    load more button update

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,10 +1,21 @@
+import clsx from "clsx";
+import Loader from '../../ui/Loader/Loader';
 import styles from "./LoadMoreBtn.module.css";
 
-export default function LoadMoreBtn({ handleChangePage }) {
+export default function LoadMoreBtn({ children,
+  isLoading,
+  onClick,
+  disabled,
+  className,
+  size }) {
   return (
     <div className={styles.wrap}>
-      <button className={styles.btn} onClick={handleChangePage}>
-        Load More
+      <button className={clsx(
+        styles.btn,
+        className,
+        isLoading ? styles.disabled : "btn-pr-effect"
+      )} disabled={isLoading || disabled} onClick={onClick}>
+        {isLoading ? <Loader size={size} /> : children}
       </button>
     </div>
   );
